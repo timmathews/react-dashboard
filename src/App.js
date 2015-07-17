f1327748293895678663f7f656383aa3a51662d0
Tim Mathews <tmathews@impaqint.com>
2015-07-16 21:28:49 -0400

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Gauge from 'react-digital-gauge';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value:33.2};
+    setInterval(()=>(this.setState({value:Math.random()*100})), 500);
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Gauge value={this.state.value} units={'m/s'} label={'Velocity'} />
+      </div>
     );
   }
 }
