41ebaea8c16de7094f49520072e655b867866213
Tim Mathews <tmathews04@gmail.com>
2015-07-20 11:55:33 -0400

    Added analog and liquid gauges

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import Gauge from 'react-digital-gauge';
+import DigitalGauge from 'react-digital-gauge';
+import AnalogGauge from 'react-analog-gauge';
+import LiquidGauge from 'react-liquid-gauge';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {value:33.2};
-    setInterval(()=>(this.setState({value:Math.random()*100})), 500);
+    setInterval(()=>(this.setState({value:Math.random()*100})), 1500);
   }
 
   render() {
+    const style = {
+      marginBottom: 10
+    };
+
     return (
-      <div>
-        <Gauge value={this.state.value} units={'m/s'} label={'Velocity'} />
+      <div style={{margin:'0 auto', width:220}}>
+        <div style={style}>
+          <DigitalGauge value={this.state.value} units={''} label={'Digital'} />
+        </div>
+        <div style={style}>
+          <AnalogGauge value={this.state.value} label={'Analog'} />
+        </div>
+        <div style={style}>
+          <LiquidGauge value={this.state.value} label={'Liquid'} />
+        </div>
       </div>
     );
   }
